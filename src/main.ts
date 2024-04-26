69b9df4124e99a25e9a917fbed6754f3f83644fd
Charlie-Brownie85 <carlosorbea85@gmail.com>
2024-04-26 19:35:51 +0200

    chore: initial setup and first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import './assets/main.css'
+import '@/css/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+import i18n from '@/i18n';
+import store from '@/stores';
+
+createApp(App).use(i18n).use(store).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
+  optimizeDeps: {
+    include: ['tailwindcss'],
+  },
   plugins: [
     vue(),
+    svgLoader(),
+    Components({
+      dirs: ['src/components', 'src/layouts'],
+      extensions: ['vue'],
+      deep: true,
+      dts: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+});
