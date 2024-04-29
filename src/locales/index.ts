8fa8b680b0010b6f05e5c5c71193c4d67748ae26
Charlie-Brownie85 <carlosorbea85@gmail.com>
2024-04-29 10:40:47 +0200

    feat: add ES translations

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,11 +1,11 @@
 import enUS from './en-US.json';
-// import esSP from './es-SP.json';
+import esSP from './es-SP.json';
 
 type Locale = Record<string, string>;
 
 const projectLocales: Record<string, Locale> = {
   en: enUS,
-  // es: esSP,
+  es: esSP,
 };
 
 export default projectLocales;
